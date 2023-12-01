d6bb510e54f13788e87325a46a3b7c07d234b088
Tiago Caixeta Deoldoto <50558787+deoldoto@users.noreply.github.com>
2023-12-01 11:38:21 -0300

    Commint inicial

---
+++ src/main/java/br/com/apitesteunitario/apitesteunitario/ApiTesteUnitarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.apitesteunitario.apitesteunitario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTesteUnitarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTesteUnitarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/apitesteunitario/apitesteunitario/ApiTesteUnitarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apitesteunitario.apitesteunitario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTesteUnitarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
