b54eb175d1b93fb5c7eb0d0949820962e73890f3
Tiago Caixeta Deoldoto <50558787+deoldoto@users.noreply.github.com>
2024-01-02 19:16:51 -0300

    Classe DTO e alteração do nome da controller

---
+++ src/main/java/br/com/apitesteunitario/apitesteunitario/dominio/dto/UsuarioDTO.java
@@ -0,0 +1,18 @@
+package br.com.apitesteunitario.apitesteunitario.dominio.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioDTO {
+    private Integer id;
+    private String nome;
+
+    private String email;
+    private String senha;
+}
