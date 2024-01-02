fbd2d31770e8ea3c0c390d2c6ba81df20f9c24ee
Tiago Caixeta Deoldoto <50558787+deoldoto@users.noreply.github.com>
2024-01-02 18:25:00 -0300

    Tratamento de excessão para objeto não encontrado

--- src/main/java/br/com/apitesteunitario/apitesteunitario/ApiTesteUnitarioApplication.java
+++ src/main/java/br/com/apitesteunitario/apitesteunitario/ApiTesteUnitarioApplication.java
@@ -1,5 +1,6 @@
 package br.com.apitesteunitario.apitesteunitario;
 
+import br.com.apitesteunitario.apitesteunitario.dominio.testeBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,9 @@ public class ApiTesteUnitarioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiTesteUnitarioApplication.class, args);
+        testeBuilder teste = testeBuilder.builder().nome("Tiago").email("deoldoto@gmail.com").build();
+        System.out.println("Nome: "+ teste.getNome());
+        System.out.println("Email: " +teste.getEmail());
     }
 
 }
