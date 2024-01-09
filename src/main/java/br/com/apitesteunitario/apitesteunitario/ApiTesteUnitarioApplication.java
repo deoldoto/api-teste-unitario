0fd2176d325ef65afad841755f66a27cf7f4acd9
Tiago Caixeta Deoldoto <50558787+deoldoto@users.noreply.github.com>
2024-01-09 14:42:33 -0300

    TEste para clase UserService método procurarPOrId

--- src/main/java/br/com/apitesteunitario/apitesteunitario/ApiTesteUnitarioApplication.java
+++ src/main/java/br/com/apitesteunitario/apitesteunitario/ApiTesteUnitarioApplication.java
@@ -1,6 +1,5 @@
 package br.com.apitesteunitario.apitesteunitario;
 
-import br.com.apitesteunitario.apitesteunitario.dominio.testeBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/test/java/br/com/apitesteunitario/apitesteunitario/service/impl/UsuarioServiceImplTest.java
@@ -0,0 +1,81 @@
+package br.com.apitesteunitario.apitesteunitario.service.impl;
+
+import br.com.apitesteunitario.apitesteunitario.config.ModelMapperConfig;
+import br.com.apitesteunitario.apitesteunitario.dominio.Usuario;
+import br.com.apitesteunitario.apitesteunitario.dominio.dto.UsuarioDTO;
+import br.com.apitesteunitario.apitesteunitario.repositories.UsuarioRespositorio;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class UsuarioServiceImplTest {
+
+    public static final Integer ID = 1;
+    public static final String NOME = "Tiago";
+    public static final String EMAIL = "deoldoto@gmail.com";
+    public static final String SENHA = "123";
+    @InjectMocks
+    private UsuarioServiceImpl service;
+    @Mock
+    private UsuarioRespositorio repositorio;
+    @Mock
+    private ModelMapperConfig mapper;
+
+    private Usuario usuario;
+    private UsuarioDTO usuarioDTO;
+    private Optional<Usuario> optionalUsuario;
+
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        inicializaUsuario();
+    }
+
+    @Test
+    void procurarPorID() {
+        when(repositorio.findById(anyInt())).thenReturn(optionalUsuario);
+
+        Usuario retorno = service.procurarPorID(ID);
+
+        assertNotNull(retorno);
+        assertEquals(Usuario.class, retorno.getClass());
+        assertEquals(ID,retorno.getId());
+        assertEquals(NOME,retorno.getNome());
+        assertEquals(EMAIL,retorno.getEmail());
+    }
+
+    @Test
+    void listarTodos() {
+    }
+
+    @Test
+    void cadastrar() {
+    }
+
+    @Test
+    void alterar() {
+    }
+
+    @Test
+    void excluir() {
+    }
+
+    private void inicializaUsuario(){
+        usuario = new Usuario(ID, NOME, EMAIL, SENHA);
+        usuarioDTO = new UsuarioDTO(ID, NOME, EMAIL, SENHA);
+        optionalUsuario = Optional.of(new Usuario(ID, NOME, EMAIL, SENHA));
+
+    }
+}
